e7c5a18d633fbb3efbdd383739ae07e7ef451bce
William Ramsey <wramsey@gmail.com>
2017-06-23 20:54:50 -0700

    Solve 5.sumBelow

--- src/recursion.js
+++ src/recursion.js
@@ -36,6 +36,9 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0) { return 0; }
+  if (n < 0) { return n + 1 + sumBelow(n + 1); }
+  else { return n - 1 + sumBelow(n - 1); }
 };
 
 // 6. Get the integers within a range (x, y).
