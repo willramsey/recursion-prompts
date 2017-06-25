7fdc7e4f2ead34033ceee2e6c539010a5cceb338
William Ramsey <wramsey@gmail.com>
2017-06-25 16:14:06 -0700

    Solve 25.fibonacci

--- src/recursion.js
+++ src/recursion.js
@@ -190,6 +190,7 @@ var rMap = function(array, callback) {
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
 var countKeysInObj = function(obj, key) {
+
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
@@ -210,6 +211,13 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
+  if (n <= 0) { return null; }
+  if (n === 1) { return [0, 1]; }
+  var returnValue = fibonacci(n - 1);
+  var len = returnValue.length;
+  var fibNum = returnValue[len - 1] + returnValue[len - 2];
+  return returnValue.concat([fibNum]);
+
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
