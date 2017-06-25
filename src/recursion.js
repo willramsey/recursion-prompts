c23c8f14789798f5437639a19663ae1cfdf04588
William Ramsey <wramsey@gmail.com>
2017-06-25 14:29:42 -0700

    Solve 15.compareStrings

--- src/recursion.js
+++ src/recursion.js
@@ -122,6 +122,11 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) { return true; }
+  if (str1.charAt(0) === str2.charAt(0)) {
+    return compareStr(str1.slice(1), str2.slice(1));
+  }
+  return false;
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
