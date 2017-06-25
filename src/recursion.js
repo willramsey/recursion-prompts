08475305aaf8dee1c796f3ccbf7e1b96a4b5666c
William Ramsey <wramsey@gmail.com>
2017-06-25 15:30:30 -0700

    Solve 20.countOccurrence

--- src/recursion.js
+++ src/recursion.js
@@ -171,6 +171,9 @@ var fizzBuzz = function(n) {
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  if (array.length === 0) { return 0; }
+  var count = array[0] === value ? 1 : 0;
+  return count + countOccurrence(array.slice(1), value);
 };
 
 // 21. Write a recursive version of map.
