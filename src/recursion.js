5e9d3a74b133ea6272562b4da0138ad9b0b9dfb3
William Ramsey <wramsey@gmail.com>
2017-06-25 14:39:40 -0700

    Solve 18.buildList

--- src/recursion.js
+++ src/recursion.js
@@ -148,6 +148,8 @@ var reverseArr = function(array) {
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
 var buildList = function(value, length) {
+  if (length === 0) { return []; }
+  return [value].concat(buildList(value, length - 1));
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
